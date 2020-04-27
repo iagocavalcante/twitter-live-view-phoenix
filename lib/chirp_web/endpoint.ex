2c1bbbedc8275c7e789dad3b48036ce8776cfd5a
Iago Cavalcante <iagoangelimc@gmail.com>
2020-04-27 12:13:34 -0300

    feat: configurando heroku

--- lib/chirp_web/endpoint.ex
+++ lib/chirp_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule ChirpWeb.Endpoint do
   ]
 
   socket "/socket", ChirpWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
